9f3cdcd71ca5632e8d98e4464efcc048baa4bc8c
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-07 13:45:29 -0300

    Criado componente 'Form'

---
+++ src/components/Form.jsx
@@ -0,0 +1,41 @@
+// CSS
+import './Form.css'
+
+const Form = ({ formName, name, email, password, confirmPassword }) => {
+    return (
+        <form className="register-form">
+            <h1>{formName}<span>.</span></h1>
+            {name && (
+                <label>
+                    Name:
+                    <input type="text" />
+                </label>
+            )}
+            {email && (
+                <label>
+                    Email:
+                    <input type="email" />
+                </label>
+            )}
+            {password && (
+                <label>
+                    Password:
+                    <input type="password" />
+                </label>
+            )}
+            {confirmPassword && (
+                <label>
+                    Confirm Password:
+                    <input type="password" />
+                </label>
+            )}
+            {formName === 'Login' ? (
+                <button className='btn'>Login</button>
+            ) : (
+                <button className='btn'>Register</button>
+            )}
+        </form>
+    )
+}
+
+export default Form
