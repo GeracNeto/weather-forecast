4896f3b6a8661472f8c6403c790d2a827ab3ac1a
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-07 13:46:02 -0300

    Atualizado App.js
    Criadas as rotas para as pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+// CSS
 import './App.css'
+
+// Libs
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// Pages
 import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+
+// Components
+
 
 function App() {
 
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Register />} /> {/* Fazer verificação se o usuario esta logado para não envia-lo para a page Register */}
+          <Route path='/about' element={<About />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
