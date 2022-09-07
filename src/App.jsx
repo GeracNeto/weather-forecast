878fa2d4e4d4330ffd72e9e85239c84ebe75888f
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-07 18:30:29 -0300

    Compartilhado contexto do usuário no sistema

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,43 @@ import About from './pages/About/About'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 
-// Components
+// Firebase
+import { onAuthStateChanged } from 'firebase/auth'
+
+// Context
+import { AuthProvider } from './context/AuthContext'
+
+// Hooks
+import { useAuthentication } from './hooks/useAuthentication'
+import { useEffect, useState } from 'react'
+
+
 
 function App() {
 
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  console.log(user)
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Register />} /> {/* Fazer verificação se o usuario esta logado para não envia-lo para a page Register */}
-          <Route path='/about' element={<About />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Register />} /> {/* Fazer verificação se o usuario esta logado para não envia-lo para a page Register */}
+            <Route path='/about' element={<About />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   )
 }
