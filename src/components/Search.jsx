e6605b0666e6c0f197f2e518a111c9a9ff05fcf1
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-08 10:11:03 -0300

    Criado componente de Search

---
+++ src/components/Search.jsx
@@ -0,0 +1,27 @@
+import './Search.css'
+
+import searchIcon from '../assets/images/search-location.png'
+import { useNavigate } from 'react-router-dom'
+
+const Search = () => {
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        navigate('/')
+
+        // fazer GET dos dados e salvar em context
+    }
+
+    return (
+        <form className='search' onSubmit={handleSubmit}>
+            <p>City:</p>
+            <input type="text" />
+            <button><img src={searchIcon} alt="search" /></button>
+        </form>
+    )
+}
+
+export default Search
