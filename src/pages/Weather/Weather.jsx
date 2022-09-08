0d4d8b66c99ac93fb30ec4508a15d2deb96375e3
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-08 14:05:37 -0300

    Atualizado page 'Weather'

--- src/pages/Weather/Weather.jsx
+++ src/pages/Weather/Weather.jsx
@@ -1,12 +1,30 @@
 import Navbar from '../../components/Navbar'
-import Home from '../Home/Home'
 import './Weather.css'
+
+import teste from '../../assets/images/rainy-day.png'
 
 const Weather = () => {
     return (
         <div className='weather'>
             <Navbar />
-            <p>teste</p>
+            <h3>
+                Thu, April 28
+            </h3>
+            <div className='data'>
+                <img src={teste} alt="teste" />
+                <p>Mostly Cloud</p>
+                <h1>22°</h1>
+                <div className='max-low'>
+                    <p>H: 24°</p>
+                    <p>L: 7°</p>
+                </div>
+                <div className='another-data'>
+                    <p>H: 24 %</p>
+                    <p>P: 1015 mbar</p>
+                    <p>W: 7 km/h</p>
+                </div>
+
+            </div>
 
         </div>
     )
