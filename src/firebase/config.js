8bab4aad3fdaaa0e12dbe8c424e8dbf95a6b2b14
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-08 17:07:33 -0300

    Exportado db da firestore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBj2Y_uU0ZYTeNEjYDgch8xxj4oC5c-4uQ",
@@ -11,5 +12,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
 
-export {app}
+export { app, db }
