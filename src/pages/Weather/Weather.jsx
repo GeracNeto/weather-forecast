873eedb2280c91069650644d762a3f72317ba49d
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-08 10:10:37 -0300

    Criada page 'Weather'

---
+++ src/pages/Weather/Weather.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../../components/Navbar'
+import Home from '../Home/Home'
+import './Weather.css'
+
+const Weather = () => {
+    return (
+        <div className='weather'>
+            <Navbar />
+            <p>teste</p>
+
+        </div>
+    )
+}
+
+export default Weather
