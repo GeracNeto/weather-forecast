dd64dfa03a8a04b36479c23fac73d583bcb1d580
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-09 14:51:17 -0300

    Criado GET das informações da cidade pesquisada

--- src/context/CityContext.jsx
+++ src/context/CityContext.jsx
@@ -1,12 +1,28 @@
+import axios from "axios";
 import { createContext, useState } from "react";
 
 export const CityContext = createContext()
 
 export const CityContextProvider = ({ children }) => {
 
-    const [city, setCity] = useState('Cahoeira de Minas')
+    const [cityData, setCityData] = useState('')
+    const [atmData, setAtmData] = useState('')
+
+    const getCityData = async (city) => {
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2412e77ba8277b67b33262f41b8ca6ff`
+
+        console.log(url)
+        axios.get(url).
+        then(response => {
+            setCityData(response.data)
+            setAtmData(response.data.main)
+        }).
+        catch(error => console.log(error))
+
+    }
 
     return (
-        <CityContext.Provider value={{ city, setCity }}>{children}</CityContext.Provider>
+        <CityContext.Provider value={{cityData, atmData, getCityData }}>{children}</CityContext.Provider>
     )
 }
