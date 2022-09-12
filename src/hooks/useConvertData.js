76bad9c7fedb636ad46b62fd276c2291d941f909
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-12 20:55:13 -0300

    Criado hook para converter dados da requisição

---
+++ src/hooks/useConvertData.js
@@ -0,0 +1,16 @@
+
+export const useConvertData = () => {
+
+    const convertDate = (unixDate) => {
+
+        let date = new Date(unixDate * 1000)
+
+        let dayName = date.toLocaleDateString("en-US", {weekday: "short"})
+        let month = date.toLocaleString("en-US", {month: "long"})
+        let dayNumber = date.toLocaleDateString("en-US", {day: "numeric"})
+        
+        return `${dayName},  ${month} ${dayNumber}`
+    }
+    
+    return { convertDate }
+}
