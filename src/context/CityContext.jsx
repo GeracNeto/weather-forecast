150dfe8ad0a9f3a1d39de1d600dcf2fced48220c
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-12 20:56:34 -0300

    Atualizado CityContext
    Atualizado nome dos dados
    Alterado unidades na URL para metric
    Criada variavel para error na requisição
    Criados novas variaveis

--- src/context/CityContext.jsx
+++ src/context/CityContext.jsx
@@ -1,28 +1,52 @@
 import axios from "axios";
 import { createContext, useState } from "react";
+import { useConvertData } from "../hooks/useConvertData";
 
 export const CityContext = createContext()
 
 export const CityContextProvider = ({ children }) => {
 
     const [cityData, setCityData] = useState('')
-    const [atmData, setAtmData] = useState('')
+    const [mainData, setMainData] = useState('')
+    const [windData, setWindData] = useState('')
+    const [date, setDate] = useState('')
+    const [description, setDescription] = useState('')
+    const [icon, setIcon] = useState('')
 
-    const getCityData = async (city) => {
+    const [error, setError] = useState(null)
 
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2412e77ba8277b67b33262f41b8ca6ff`
+    const getCityData = (city) => {
+
+        const { convertDate } = useConvertData()
+
+        // Resgatar API key do usuário no firestore e colocar na URL aqui
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2412e77ba8277b67b33262f41b8ca6ff&units=metric`
 
         console.log(url)
         axios.get(url).
-        then(response => {
-            setCityData(response.data)
-            setAtmData(response.data.main)
-        }).
-        catch(error => console.log(error))
+            then(response => {
 
+                const convertedDate = convertDate(response.data.dt)
+
+                setCityData(response.data)
+                setMainData(response.data.main)
+                setWindData(response.data.wind)
+                setDate(convertedDate)
+                setDescription(response.data.weather[0].description)
+                setIcon(response.data.weather[0].icon)
+
+                setError(null)
+            }).
+            catch(error => {
+                
+                if(error.message.includes('404')){
+                    setError(`City: '${city}' not found...`)
+                }
+            })
     }
 
     return (
-        <CityContext.Provider value={{cityData, atmData, getCityData }}>{children}</CityContext.Provider>
+        <CityContext.Provider value={{ cityData, mainData, windData, date, error, getCityData, description, icon }}>{children}</CityContext.Provider>
     )
 }
