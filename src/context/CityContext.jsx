e2d6a94649e98dbe43a5dc4f15be940969f6a4e6
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-14 20:51:32 -0300

    Atualizado CityContext
    Criado condição para resgatar apiKey do usuário no firestore e aplica-la no GET dos dados da API
    Criada condição de erro para apiKey inválidas

--- src/context/CityContext.jsx
+++ src/context/CityContext.jsx
@@ -2,9 +2,17 @@ import axios from "axios";
 import { createContext, useState } from "react";
 import { useConvertData } from "../hooks/useConvertData";
 
+import { db } from "../firebase/config";
+import { useEffect } from "react";
+import { collection, doc, getDocs } from "firebase/firestore";
+import { useAuthValue } from "./AuthContext";
+
 export const CityContext = createContext()
 
 export const CityContextProvider = ({ children }) => {
+
+    const { convertDate } = useConvertData()
+    const { user } = useAuthValue()
 
     const [cityData, setCityData] = useState('')
     const [mainData, setMainData] = useState('')
@@ -13,15 +21,32 @@ export const CityContextProvider = ({ children }) => {
     const [description, setDescription] = useState('')
     const [icon, setIcon] = useState('')
 
+    const [userKey, setUserKey] = useState('')
+
     const [error, setError] = useState(null)
+
+    // Resgatar API key do usuário no firestore e colocar na URL aqui
+    const userCollectionRef = collection(db, "weatherkey")
+
+    useEffect(() => {
+
+        const getUserKey = async () => {
+
+            const data = await getDocs(userCollectionRef)
+            let usersData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+
+            usersData.filter(item => {
+                if (item.email === user.email) {
+                    setUserKey(item.apiKey)
+                }
+            })
+        }
+        getUserKey()
+    })
 
     const getCityData = (city) => {
 
-        const { convertDate } = useConvertData()
-
-        // Resgatar API key do usuário no firestore e colocar na URL aqui
-
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2412e77ba8277b67b33262f41b8ca6ff&units=metric`
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${userKey}&units=metric`
 
         console.log(url)
         axios.get(url).
@@ -39,14 +64,17 @@ export const CityContextProvider = ({ children }) => {
                 setError(null)
             }).
             catch(error => {
-                
-                if(error.message.includes('404')){
+
+                if (error.message.includes('404')) {
                     setError(`City: '${city}' not found...`)
+                }
+                else if (error.message.includes('401')) {
+                    setError('User Key not Valid')
                 }
             })
     }
 
     return (
-        <CityContext.Provider value={{ cityData, mainData, windData, date, error, getCityData, description, icon }}>{children}</CityContext.Provider>
+        <CityContext.Provider value={{ cityData, mainData, windData, date, error, setError, getCityData, description, icon, setIcon }}>{children}</CityContext.Provider>
     )
 }
