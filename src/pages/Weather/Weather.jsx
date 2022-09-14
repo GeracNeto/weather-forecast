eef95adc08166dbfaae047d3c4ceb1dd53de9928
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-14 20:52:10 -0300

    Criada condição para exibir os dados apenas quando o usuário pesquisar o nome da cidade

--- src/pages/Weather/Weather.jsx
+++ src/pages/Weather/Weather.jsx
@@ -7,32 +7,40 @@ import { CityContext } from '../../context/CityContext'
 const Weather = () => {
 
     // get city name from context
-    const { cityData, mainData, windData, date, description, icon, error } = useContext(CityContext)
-    
+
+    const { cityData, mainData, windData, date, description, error, setError, icon, setIcon} = useContext(CityContext)
+
+    useEffect(() => {
+        setIcon(null)
+        setError(null)
+    }, [])
+
     return (
         <div className='weather'>
             <Navbar />
-            <h3 id='day'>
-                {date}
-            </h3>
             {icon ? (
-                <div className='data'>
-                <h2>
-                    {cityData.name}
-                </h2>
-                <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather-icon" />
-                <p>{description}</p>
-                <h1>{mainData.temp} °C</h1>
-                <div className='max-low'>
-                    <p>H: {mainData.temp_max} °C</p>
-                    <p>L: {mainData.temp_min} °C</p>
-                </div>
-                <div className='another-data'>
-                    <p>H: {mainData.humidity} %</p>
-                    <p>P: {mainData.pressure} hPa</p>
-                    <p>W: {windData.speed} km/h</p>
-                </div>
-            </div>
+                <>
+                    <h3 id='day'>
+                        {date}
+                    </h3>
+                    <div className='data'>
+                        <h2>
+                            {cityData.name}
+                        </h2>
+                        <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather-icon" />
+                        <p>{description}</p>
+                        <h1>{mainData.temp} °C</h1>
+                        <div className='max-low'>
+                            <p>H: {mainData.temp_max} °C</p>
+                            <p>L: {mainData.temp_min} °C</p>
+                        </div>
+                        <div className='another-data'>
+                            <p>H: {mainData.humidity} %</p>
+                            <p>P: {mainData.pressure} hPa</p>
+                            <p>W: {windData.speed} km/h</p>
+                        </div>
+                    </div>
+                </>
             ) : (
                 <div className='data'>
                     <h2>Search a city to GET data ...</h2>
@@ -41,7 +49,6 @@ const Weather = () => {
             {error && (
                 <p className='error'>{error}</p>
             )}
-
         </div>
     )
 }
